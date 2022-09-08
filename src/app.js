d5a028edcb25fc893b8b01d90a2c50dc1b86e815
estefaniggt <estefanigabri@gmail.com>
2022-09-08 04:16:30 +0000

    Cree un script que genera todas las combinaciones posibles de nombres de dominio

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["el", "ella", "su", "tu", "nuestro", "mi"];
+  let adj = [
+    "amable",
+    "responsable",
+    "sincero",
+    "generoso",
+    "afectuosa",
+    "simpático"
+  ];
+  let noun = ["perro", "lujuria", "botella", "reloj", "taladro", "pez"];
+  let dotSom = [".com", ".ve", ".lov", ".devs", ".my", ".net"];
+
+  let excuseP = document.querySelector("#excuse");
+  excuseP.textContent =
+    pronoun[Math.floor(Math.random() * 6)] +
+    adj[Math.floor(Math.random() * 6)] +
+    noun[Math.floor(Math.random() * 6)] +
+    dotSom[Math.floor(Math.random() * 6)];
+
+  console.log(excuseP);
 };
