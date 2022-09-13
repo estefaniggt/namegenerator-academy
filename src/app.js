18b46b9fba483a88fa1eb517018ab9b8f102b4f5
estefaniggt <estefanigabri@gmail.com>
2022-09-13 16:23:00 +0000

    Agregue un for en mi funcion que recorre cada una de las variables y luego se ven en consola.

--- src/app.js
+++ src/app.js
@@ -19,12 +19,22 @@ window.onload = function() {
   let noun = ["perro", "lujuria", "botella", "reloj", "taladro", "pez"];
   let dotSom = [".com", ".ve", ".lov", ".devs", ".my", ".net"];
 
-  let excuseP = document.querySelector("#excuse");
-  excuseP.textContent =
-    pronoun[Math.floor(Math.random() * 6)] +
-    adj[Math.floor(Math.random() * 6)] +
-    noun[Math.floor(Math.random() * 6)] +
-    dotSom[Math.floor(Math.random() * 6)];
-
-  console.log(excuseP);
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let ds = 0; ds < dotSom.length; ds++) {
+          console.log(pronoun[p] + adj[a] + noun[n] + dotSom[ds]);
+        }
+      }
+    }
+  }
 };
+
+//let excuseP = document.querySelector("#excuse");
+//excuseP.textContent =
+//pronoun[Math.floor(Math.random() * 6)] +
+// adj[Math.floor(Math.random() * 6)] +
+//noun[Math.floor(Math.random() * 6)] +
+//dotSom[Math.floor(Math.random() * 6)];
+
+//console.log(excuseP);
